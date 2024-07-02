f827f839a669c2ff3b1f6fd75718860d9e844721
wdavidlbello <wdavidlbello@gmail.com>
2024-07-02 00:17:01 -0400

    creating html templates

--- hello/views.py
+++ hello/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world")
+    return render(request,"hello/index.html")
+
+def brian(request):
+    return HttpResponse("Hello Brian!")
+
+def greet (request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
